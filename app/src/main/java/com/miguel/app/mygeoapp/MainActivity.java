91012bda5f4c058a06f67b2d52b65188132c6f8e
Miguel C <webdazer0@gmail.com>
2021-02-25 15:17:42 +0100

    ...

---
+++ app/src/main/java/com/miguel/app/mygeoapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.miguel.app.mygeoapp;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        MyLocation myLocation = new MyLocation(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 1, myLocation);
+
+            Toast.makeText(this, "HELLO", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Devi abilitare GPS", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/miguel/app/mygeoapp/MyLocation.java
@@ -0,0 +1,47 @@
+package com.miguel.app.mygeoapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+public class MyLocation implements LocationListener {
+
+    Context context;
+    LayoutInflater inflater;
+    Activity activity;
+
+    public MyLocation(Context context) {
+        this.context = context;
+        this.inflater = LayoutInflater.from(this.context);
+        activity = (Activity)(this.context);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        TextView txtPosition = (TextView)activity.findViewById(R.id.txtPosition);
+        txtPosition.setText("Lat: " + location.getLatitude() + " | Long: " + location.getLongitude());
+
+        Log.i("MITO_DEBUG", "Lat: " + location.getLatitude() + " | Long: " + location.getLongitude());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+}
